d23d9aefb16f33153ba9f8c2f3ca36767019df9b
Maarten de Vries <maarten@de-vri.es>
2019-04-26 00:11:59 +0200

    Add tests for shared context.

---
+++ inline-python/tests/shared_context.rs
@@ -0,0 +1,30 @@
+#![feature(proc_macro_hygiene)]
+
+use inline_python::{python, pyo3};
+
+#[test]
+fn continue_context() {
+	let context = inline_python::Context::new();
+	python!{
+		#![context = &context]
+		foo = 5
+	}
+	python!{
+		#![context = &context]
+		assert foo == 5
+	}
+}
+
+#[test]
+fn extract_global() {
+	let context = inline_python::Context::new();
+	python!{
+		#![context = &context]
+		foo = 5
+	}
+
+	let gil = pyo3::Python::acquire_gil();
+	let py  = gil.python();
+
+	assert_eq!(context.get_global(py, "foo").unwrap(), Some(5));
+}
