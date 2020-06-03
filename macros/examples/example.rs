45c3dca0f17b3cbab85d35ebc2ea4a518d503961
Mara Bos <m-ou.se@m-ou.se>
2020-06-03 19:43:13 +0200

    Update ct_python example.

--- macros/examples/example.rs
+++ macros/examples/example.rs
@@ -2,17 +2,20 @@
 
 use inline_python_macros::ct_python;
 
-static DIRECTIONS: [(f64, f64); 32] = ct_python! {
+ct_python! {
+	print("static A: i32 = 1;")
+}
+
+static DIRECTIONS: [(f64, f64); 4] = ct_python! {
 	from math import sin, cos, tau
-	n = 32
+	n = 4
 	print("[")
 	for i in range(n):
-		x = cos(i / n * tau)
-		y = sin(i / n * tau)
-		print(f"({x}, {y}),")
+		print(f"({cos(i / n * tau)}, {sin(i / n * tau)}),")
 	print("]")
 };
 
 fn main() {
+	dbg!(&A);
 	dbg!(&DIRECTIONS);
 }
